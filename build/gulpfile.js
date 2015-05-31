8475d1d7bac98bde5260c89702508d1dd2a2fc47
Erik Sälgström Peterson <erik@swedenunlimited.com>
2015-05-31 15:37:48 -0400

    Add build task for SASS, directory for SASS source

---
+++ build/gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var env = process.env.NODE_ENV || 'dev';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var maps = require('gulp-sourcemaps');
+
+var path = require('path');
+var sasspath = path.resolve(__dirname, '../wp-content/themes/dikeou-collection/static/src/scss');
+var csspath = path.resolve(__dirname, '../wp-content/themes/dikeou-collection/static/css');
+
+gulp.task('sass', function(){
+	if(env === 'dev'){
+		return gulp.src(path.join(sasspath, 'style.scss'))
+					.pipe(maps.init())
+						.pipe(sass())
+						.pipe(maps.write('./maps'))
+						.pipe(gulp.dest(path.join(csspath)));
+	}
+
+	return gulp.src(path.join(sasspath, 'style.scss'))
+				.pipe(sass())
+				.pipe(gulp.dest(path.join(csspath)));
+});
