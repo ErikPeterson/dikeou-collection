475adabbe58893eceddcbee4ce43b94a0b1fc2ce
ErikPeterson <erik@swedenunlimited.com>
2016-02-13 17:11:17 -0500

    Fix up artco head thing

--- wp-content/themes/dikeou-collection/artco-posts.php
+++ wp-content/themes/dikeou-collection/artco-posts.php
@@ -1,8 +1,8 @@
 <?php
 $posts = Timber::get_posts();
 $context = Timber::get_context();
-
 $context['posts'] = $posts;
 $context['page_type'] = 'art-co';
+$context['page'] = Timber::get_post(array('pagename'=>'artco-page'));
 
 Timber::render('artco-posts.twig', $context);
